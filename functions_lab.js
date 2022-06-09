c3b293e0063096a16243e78d6d3ef9058f9e88f7
Sarah Ariyo <sarahariyo@hotmail.co.uk>
2022-06-09 11:58:17 +0100

    adding files

---
+++ functions_lab.js
@@ -0,0 +1,106 @@
+// Create the following functions and test them by logging out their return value.
+
+// MVP
+
+// Part 1
+
+var myNumbers = [1,2,3,4,5,6,7,8]
+
+// 1.a Total the numbers - Create a function that takes in an array of numbers as an argument and calculates the sum of the array's numbers.
+
+function getTotalOfArray(numbers){
+    
+    var total = 0
+    
+    for (var number of numbers) {
+      
+        total += number
+    }
+    // return the result
+    return total
+}
+console.log("total is:", getTotalOfArray(myNumbers))
+
+
+// 1.b Find the evens - Create a function that takes in an array of numbers as an argument and returns a new array containing just the even numbers.
+
+var evenNumbers = myNumbers.filter(function(number){
+    return number % 2 === 0;
+})
+console.log(evenNumbers)
+
+
+
+
+// Part 2
+
+var myBill = [
+  {
+    meal: 'Green Curry',
+    price: 10,
+    customer: 'Sally'
+  }, {
+    meal: 'Sea Bass',
+    price: 12,
+    customer: 'Upul'
+  }, {
+    meal: 'Fish and Chips',
+    price: 10,
+    customer: 'Rita'
+  }, {
+    meal: 'Stick Toffee Pudding',
+    price: 4,
+    customer: 'Upul'
+  }, {
+    meal: 'Apple Pie',
+    price: 5,
+    customer: 'Sally'
+  }
+]
+
+// 2.a. Total the bill - Create a function that takes in a bill as an argument and calculates the total cost of the bill.
+
+function getTotalBill(myBill){
+  var total = 0
+  for (var item of myBill){
+    var itemPrice = item.price
+    total += itemPrice
+  }
+  return total
+}
+console.log("total price:", getTotalBill(myBill))
+
+// 2.b. Find meals by customer name - Create a function that takes in a bill and a customer name as an argument and returns a new array containing all the items for that customer.
+
+function getIndividualBill(myBill){
+  for (var currentBillObj of myBill){
+      var customerNameToFind = "Sally"
+      var customerMeal = currentBillObj.meal
+      if (customerNameToFind === "Sally")
+      console.log("Sally had:", customerMeal(myBill))
+  }
+}
+
+
+// 2.c. Using the functions you wrote in part 4 and 5, find the total that Sally spent.
+
+
+// 2.d. Find average item price - Create a function that takes in bill as an argument and calculates the average price of all the items.
+
+// Extension
+
+// Part 3
+// Have a look at the following bodyBuilder object.
+
+var bodyBuilder = {
+  strength: 2,
+  workOut: (mins) => {
+   bodyBuilder.strength = bodyBuilder.strength * mins;
+  }
+}
+
+// 3.a. Log out the the bodyBuilder's strength - the value should be 2.
+
+// 3.b. Use the workOut function to update the bodyBuilder's strenth.
+
+// 3.c. Create another function on the bodyBuilder called `lazeAbout`, that takes in a number of hours as an argument. Decrease the bodyBuilder's strength by dividing the strength by the number of hours.
